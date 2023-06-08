1bfd635301f491bb1c9f5647dfe579c9fc47a7a7
Temur_Zhakibaev <temurzhakibaev@gmail.com>
2023-06-08 04:17:20 -0700

    xz

--- src/redux/usersSlice/userSlice.js
+++ src/redux/usersSlice/userSlice.js
@@ -1,59 +1,66 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { addPost, getAllUsers, deleteItemId } from "../extraReducer/extraReducer";
+import {
+	addPost,
+	getAllUsers,
+	deleteItemId,
+} from "../extraReducer/extraReducer";
 const initialState = {
-    usersData: [],
-    loading: false,
-    error: null,
-    onuserAdded: "",
-    deleteAction: ''
-}
+	usersData: [],
+	loading: false,
+	error: null,
+	onuserAdded: "",
+	deleteAction: "",
+};
 const userSlice = createSlice({
-    name: "users",
-    initialState,
-    reducers: {
-        handleDeleteFood: (state, action) => {
-            state.usersData = state.usersData.filter(el =>el.id !== action.payload)
-        }
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(addPost.pending, (state, action) => {
-                state.loading = true;
-                state.onuserAdded = "pending"
-            })
-            .addCase(addPost.fulfilled, (state, action) => {
-                state.loading = false;
-                state.onuserAdded = 'fullfiled'
-            })
-            .addCase(addPost.rejected, (state, action) => {
-                state.error = action.error.message
-            })
-        ///////////get users////////
-        builder
-            .addCase(getAllUsers.pending, (state, action) => {
-                state.loading = true;
-            })
-            .addCase(getAllUsers.fulfilled, (state, action) => {
-                state.loading = false;
-                state.usersData = action.payload;
-            })
-            .addCase(getAllUsers.rejected, (state, action) => {
-                state.error = action.error.message
-            })
-        /////////delete method/////
-        builder
-            .addCase(deleteItemId.pending, (state) => {
-                state.loading = true;
-                state.deleteAction = 'pending delete'
-            })
-            .addCase(deleteItemId.fulfilled, (state, action) => {
-                state.loading = false;
-                state.deleteAction = 'deleted Succsess'
-            })
-            .addCase(deleteItemId.rejected, (state, action) => {
-                state.error = action.error.message
-            })
-    }
-})
-export const { handleDeleteFood} = userSlice.actions
-export default userSlice.reducer
+	name: "users",
+	initialState,
+	reducers: {
+		handleDeleteFood: (state, action) => {
+			state.usersData = state.usersData.filter(
+				(el) => el.id !== action.payload
+			);
+		},
+	},
+
+	extraReducers: (builder) => {
+		builder
+			.addCase(addPost.pending, (state, action) => {
+				state.loading = true;
+				state.onuserAdded = "pending";
+			})
+			.addCase(addPost.fulfilled, (state, action) => {
+				state.loading = false;
+				state.onuserAdded = "fullfiled";
+			})
+			.addCase(addPost.rejected, (state, action) => {
+				state.error = action.error.message;
+			});
+		///////////get users////////
+		builder
+			.addCase(getAllUsers.pending, (state, action) => {
+				state.loading = true;
+			})
+			.addCase(getAllUsers.fulfilled, (state, action) => {
+				state.loading = false;
+				state.usersData = action.payload;
+			})
+			.addCase(getAllUsers.rejected, (state, action) => {
+				state.error = action.error.message;
+			});
+		/////////delete method/////
+		builder
+			.addCase(deleteItemId.pending, (state) => {
+				state.loading = true;
+				state.deleteAction = "pending delete";
+			})
+			.addCase(deleteItemId.fulfilled, (state, action) => {
+				state.loading = false;
+				state.deleteAction = "deleted Succsess";
+			})
+			.addCase(deleteItemId.rejected, (state, action) => {
+				state.error = action.error.message;
+			});
+	},
+});
+export const { handleDeleteFood } = userSlice.actions;
+export default userSlice.reducer;
